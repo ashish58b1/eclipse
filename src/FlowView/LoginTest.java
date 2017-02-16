824068fbb7d41fb6e6c55840338bed4612e5e85b
Ashish Srivastava <www.ashish06@gmail.com>
2017-02-16 16:16:09 +0530

    this is testing

---
+++ src/FlowView/LoginTest.java
@@ -0,0 +1,27 @@
+package FlowView;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().deleteAllCookies();
+		driver.get("http://fv3test.azurewebsites.net/login");
+	
+		//driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.findElement(By.id("userName")).sendKeys("flname1");
+		driver.findElement(By.id("userPassword")).sendKeys("Temp~1");
+		driver.findElement(By.id("userPassword")).sendKeys("Temp~1");
+		driver.findElement(By.xpath("//div[@class='pull-right login-button']")).click();
+
+	}
+
+}
